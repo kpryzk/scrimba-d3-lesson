dfb7c875a90646c25eab5b380fcd983dad9dd7ce
Katherine Pryszlak <kepryzk@gmail.com>
2018-01-22 15:11:09 -0800

    notes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var data = [30, 86, 168, 281, 303, 465];
 
+
+//scales graph to fit within a specified space (325 per .css) must list domain that contains highest number and also how much space it should occupy
 var scale = d3.scale.linear()
   .domain([0, 465])
   .range([0, 310])
